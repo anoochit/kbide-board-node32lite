e806b2018ef58ca0ab08da6c6aa36a8f2cbb2e0d
Anuchit Chalothorn <anoochit@gmail.com>
2019-06-15 16:07:06 +0700

    node32lite board

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {    
-    name : 'esp32-wrover',
+    name : 'node32lite',
     platform : 'arduino-esp32',
-    title : 'Generic ESP32 Wrover',
-    description : 'ESP32-Wrover development board with extra RAM.\n',
-    author : 'Comdet Phueadphut',
+    title : 'Node32lite',
+    description : 'Node32lite development board with extra RAM.\n',
+    author : 'Anuchit Chalothorn',
     website : 'https://docs.espressif.com/projects/esp-idf/en/latest/get-started/get-started-wrover-kit.html',
-    email : 'comdet.p@gmail.com',
-    git : 'https://github.com/comdet/generic-esp32-wrover/',
+    email : 'anoochit@gmail.com',
+    git : 'https://github.com/anoochit/kbide-board-node32lite/',
     image : '/static/display.jpg',
-    version : '1.0.5'
+    version : '1.0.0'
 };
